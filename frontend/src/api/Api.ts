72b5a7998428d9727b1278a60584bc949eb196ee
Paul Bader <paul.bader@rock5.de>
2023-11-22 22:28:42 +0100

    add session handling to frontend api

--- frontend/src/api/Api.ts
+++ frontend/src/api/Api.ts
@@ -13,8 +13,32 @@ export default class Api {
     return this.instance;
   }
 
+  private static writeAccessToken(access_token: string): void {
+    localStorage.setItem("access_token", access_token);
+  }
+
+  private static getAccessToken(): string | null {
+    return localStorage.getItem("access_token");
+  }
+
+  private static removeAccessToken() {
+    localStorage.removeItem("access_token");
+  }
+
+  private static authHeader(): { Authorization: string } | undefined {
+    const access_token = this.getAccessToken();
+
+    if (access_token) {
+      console.log(access_token);
+      return { Authorization: `Bearer ${access_token}` };
+    }
+  }
+
   private async get(endpoint: string) {
-    return await fetch(Api.API_PATH + endpoint, { method: "GET" });
+    return await fetch(Api.API_PATH + endpoint, {
+      method: "GET",
+      headers: Api.authHeader(),
+    });
   }
 
   private async post(endpoint: string, data: Object) {
@@ -23,15 +47,39 @@ export default class Api {
     return await fetch(Api.API_PATH + endpoint, {
       method: "POST",
       body: JSON.stringify(data),
-      headers: { "Content-Type": "application/json" },
+      headers: { "Content-Type": "application/json", ...Api.authHeader() },
     });
   }
 
-  public static login(username: string, password: string) {
-    return Api.getInstance().post("auth/login", { username, password });
+  public static async login(username: string, password: string): Promise<any> {
+    return await Api.getInstance()
+      .post("auth/login", { username, password })
+      .then(async (res) => {
+        if (res.status >= 200 && res.status < 300) {
+          console.log("successfully logged in");
+
+          this.writeAccessToken((await res.json()).access_token);
+        }
+      });
   }
 
-  public static register(username: string, password: string) {
-    return Api.getInstance().post("auth/register", { username, password });
+  public static async register(username: string, password: string) {
+    return await Api.getInstance()
+      .post("auth/register", { username, password })
+      .then(async (res) => {
+        // on successfull registration, directly login the user
+        if (res.status >= 200 && res.status < 300) {
+          return await this.login(username, password);
+        }
+      });
+  }
+
+  public static logout() {
+    // TODO make api call to auth/logout
+    this.removeAccessToken();
+  }
+
+  public static async getHabits() {
+    return await this.getInstance().get("habits");
   }
 }
--- frontend/src/routes/login/Login.tsx
+++ frontend/src/routes/login/Login.tsx
@@ -1,9 +1,10 @@
+import "../login/Login.css";
+
 import { Button, Paper, TextField } from "@mui/material";
 import { ChangeEvent, useState } from "react";
 
 import Api from "../../api/Api";
 import { ColumnFlexBox } from "../../components/FlexBox";
-import "../login/Login.css"
 
 export default function Login() {
   const [username, setUsername] = useState<string>("");
@@ -18,45 +19,46 @@ export default function Login() {
   };
 
   const onLoginButtonPressed = () => {
-    Api.login(username, password).then(async (res) => {
-      if (res.status >= 200 && res.status < 300) {
-        console.log("successfully logged in");
-        console.log(res.body);
-        console.log(await res.json());
-      }
-    });
+    Api.login(username, password);
   };
 
   return (
     <div className="LoginWindow">
-    <Paper elevation={7} square={false} className="paper">
-      <h2 className="header">Habittracking Login</h2>
-      <ColumnFlexBox>
-        <TextField style={{margin: "10px"}}
-          onChange={onUsernameChange}
-          label="Username"
-        ></TextField>
-        <TextField style={{margin: "10px"}}
-          onChange={onPasswordChange}
-          label="Password"
-        ></TextField>
-        <Button 
-          style={{margin: "10px"}}
-          variant="contained"
-          onClick={onLoginButtonPressed}
-        >
-          Login
-        </Button>
-        <Button
-          style={{margin: "10px"}}
-          variant="contained"
-          onClick={() => {
-            window.location.href = "/register";
-          }}>
-          Register
+      <Paper
+        elevation={7}
+        square={false}
+        className="paper"
+      >
+        <h2 className="header">Habittracking Login</h2>
+        <ColumnFlexBox>
+          <TextField
+            style={{ margin: "10px" }}
+            onChange={onUsernameChange}
+            label="Username"
+          ></TextField>
+          <TextField
+            style={{ margin: "10px" }}
+            onChange={onPasswordChange}
+            label="Password"
+          ></TextField>
+          <Button
+            style={{ margin: "10px" }}
+            variant="contained"
+            onClick={onLoginButtonPressed}
+          >
+            Login
           </Button>
-      </ColumnFlexBox>
-    </Paper>
+          <Button
+            style={{ margin: "10px" }}
+            variant="contained"
+            onClick={() => {
+              window.location.href = "/register";
+            }}
+          >
+            Register
+          </Button>
+        </ColumnFlexBox>
+      </Paper>
     </div>
   );
 }
