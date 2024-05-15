9426a1c037df7471bcd2d17b1fa4c630079c7db6
hallowelt100 <kayra.gueler32@gmail.com>
2024-05-15 09:35:42 +0200

    Added testing for login page navigation

--- frontend/src/__tests__/Login.test.tsx
+++ frontend/src/__tests__/Login.test.tsx
@@ -3,9 +3,20 @@ import { render, screen } from '@testing-library/react';
 import Login from '../routes/login/Login';
 import { BrowserRouter } from 'react-router-dom';
 import userEvent from '@testing-library/user-event';
+import Registration from '../routes/registration/Registration';
 
-test('renders the site correctly', () => {
+test('Renders the site correctly', () => {
      render(<Login />, {wrapper: BrowserRouter})
 
      expect(screen.getByText(/Habittracking Login/i)).toBeInTheDocument()
-    });
+});
+
+test('Navigates to register', async () => {
+     render(<Login />, {wrapper: BrowserRouter})
+
+     await userEvent.click(screen.getByText(/Register/i))
+     render(<Registration />, {wrapper: BrowserRouter})
+
+
+     expect(screen.getByText(/Habittracking Registration/i)).toBeInTheDocument()
+});
