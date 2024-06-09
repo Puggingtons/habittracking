d524ad6dfe3a1b723ae8a488ac77dcf77b6538df
Paul Bader <paul@pbdr.de>
2024-06-09 12:00:16 +0200

    add color to delete button

--- frontend/src/components/DeleteHabit.tsx
+++ frontend/src/components/DeleteHabit.tsx
@@ -24,6 +24,7 @@ export default function DeleteHabit(props: Readonly<{ habit: Habit }>) {
   return (
     <>
       <IconButton
+        color="error"
         onClick={() => {
           onClick();
         }}
